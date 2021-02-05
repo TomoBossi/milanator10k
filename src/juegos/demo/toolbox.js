572de57857ee996d4fbcfbe01bbb0dbe03c4cc69
Ralo <gpfernandezflorio@gmail.com>
2021-02-05 16:17:55 -0300

    Agrego categorías al toolbox

--- src/juegos/demo/toolbox.js
+++ src/juegos/demo/toolbox.js
@@ -1,13 +1,23 @@
 // Toolbox para el juego Demo
 Juego.toolbox =
   '<xml>\
-    <block type="controls_if"></block>\
-    <block type="logic_boolean"></block>\
-    <block type="controls_repeat_ext"></block>\
-    <block type="math_number"></block>\
-    <block type="derecha"></block>\
-    <block type="izquierda"></block>\
-    <block type="arriba"></block>\
-    <block type="abajo"></block>\
-    <block type="robot_def"></block>\
+    <category name="Primitivas">\
+      <block type="robot_def"></block>\
+      <block type="derecha"></block>\
+      <block type="izquierda"></block>\
+      <block type="arriba"></block>\
+      <block type="abajo"></block>\
+    </category>\
+    <category name="Control">\
+      <block type="controls_if"></block>\
+      <block type="controls_repeat_ext"></block>\
+    </category>\
+    <category name="Valores">\
+      <block type="logic_boolean"></block>\
+      <block type="math_number"></block>\
+    </category>\
+    <category name="Definiciones">\
+      <block type="procedures_defnoreturn"></block>\
+      <block type="procedures_defreturn"></block>\
+    </category>\
   </xml>';

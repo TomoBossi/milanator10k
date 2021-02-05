837fd6d19fbbe1661609cd526d78fb0d3704aa4d
Ralo <gpfernandezflorio@gmail.com>
2021-02-05 12:57:53 -0300

    Guardo el workspace tras cada cambio para poder restaurarlo al recargar la página

--- src/blockly.js
+++ src/blockly.js
@@ -8,6 +8,9 @@
     inicializar             Inicializa el entorno Blockly
       inyectarBlockly       Inyecta la interfaz Blockly en la div con id "blockly" y guarda el resultado en Mila.workspace
 
+    generarXml              Genera el xml de un workspace en formato string
+    cargarDesdeXml          Crea los bloques en un workspace a partir de un xml en formato string
+
 **/
 
 Mila.Blockly = {};
@@ -15,7 +18,18 @@ Mila.Blockly = {};
 // Inicializa el entorno Blockly
 Mila.Blockly.inicializar = function() {
   Mila.Blockly.inyectarBlockly();   // Inyectar la interfaz de Blockly
-}
+  // Agrego un listener de eventos para guardar el workspace tras cada cambio
+  Mila.workspace.addChangeListener(function(evento) {
+    sessionStorage.xml = Mila.Blockly.generarXml(Mila.workspace);
+  });
+  // Me fijo si hay un workspace guardado
+  if (sessionStorage.xml) {
+    Mila.Blockly.cargarDesdeXml(Mila.workspace, sessionStorage.xml);
+  // Si no, creo un workspace vacío
+  } else {
+    // Crear bloque inicial
+  }
+};
 
 // Inyecta la interfaz Blockly en la div con id "blockly" y guarda el resultado en Mila.workspace
 Mila.Blockly.inyectarBlockly = function() {
@@ -23,4 +37,14 @@ Mila.Blockly.inyectarBlockly = function() {
   Mila.workspace = Blockly.inject('blockly', {
     toolbox: Juego.toolbox                      // Set de bloques del juego actual
   });
+};
+
+// Genera el xml de un workspace en formato string
+Mila.Blockly.generarXml = function(workspace) {
+  return Blockly.Xml.domToText(Blockly.Xml.workspaceToDom(workspace));
+};
+
+// Crea los bloques en un workspace a partir de un xml en formato string
+Mila.Blockly.cargarDesdeXml = function(workspace, xml) {
+  Blockly.Xml.domToWorkspace(Blockly.Xml.textToDom(xml), workspace);
 };

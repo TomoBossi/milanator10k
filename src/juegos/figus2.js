5ed7d7be0bcc88f8256c48e12acb8ef3da6803f6
gpfernandezflorio <gpfernandezflorio@gmail.com>
2023-04-29 22:32:25 -0300

    Completo implementación de figus 2

--- src/juegos/figus2.js
+++ src/juegos/figus2.js
@@ -25,46 +25,52 @@ Juego.tiemposBloque = {
   controls_repeat_ext:2,
   math_number:2,
   logic_boolean:2,
-  math_on_list_figus:4,
-  logic_compare_figus:2,
-  math_arithmetic:2,
-  math_random_int:4
+  math_on_list_figus:1,
+  logic_compare_figus:1,
+  math_arithmetic:1,
+  math_random_int:2
 };
 
 Juego.acciones = [
   'var_assign','album','figuActual','contador','anotador',
-  'listaVacia','lista6Ceros','list_assign','list_push',
+  'list_assign','list_push',
   'decir'
 ];
 
 Mila.generador.header = 'var var_album = undefined;\nvar var_anotador = undefined;\nvar var_figuActual = undefined;\nvar var_contador = undefined;\n';
 
+Mila.generador.listaVacia = function(bloque) {
+  return ["[]",0];
+};
+Mila.generador.lista6Ceros = function(bloque) {
+  return ["[0,0,0,0,0,0]",0];
+};
 for (let a of Juego.acciones) {
   Mila.generador.addReservedWords(a);
   Mila.generador[a] = function(bloque){
     if (['album','figuActual','contador','anotador'].includes(a)) {
-      return [`var_${a}`,0];
-    }
-    if (a == 'listaVacia') {
-      return ["[]",0];
-    }
-    if (a == 'lista6Ceros') {
-      return ["[0,0,0,0,0,0]",0];
+      return [`(${a}()||var_${a})`,0];
     }
     let args = Juego.argsBloque(bloque);
     if (a == 'var_assign') {
-      return `${bloque.getFieldValue('VAR')} = ${args};\n`;
+      let v = bloque.getFieldValue('VAR');
+      let arg_b = Juego.argsBloqueJuego(bloque, v);
+      return `${v} = ${(args||"undefined")};\nvar_assign({v:"${v}",a:${arg_b}});\n`;
     }
     if (a == 'list_assign') {
+      let v = bloque.getFieldValue('VAR');
       let pos = Mila.generador.valueToCode(bloque, "POS", 0);
-      return `${bloque.getFieldValue('VAR')}[${pos}] = ${args};\n`;
+      let arg_b = Juego.argsBloqueJuego(bloque, args);
+      return `list_assign({v:"${v}",p:${pos},a:${arg_b}});\n${v}[${pos}] = ${args};\n`;
     }
     if (a == 'list_push') {
-      return `${bloque.getFieldValue('VAR')}.push(${args});\n`;
-    }
+      let v = bloque.getFieldValue('VAR');
+      let arg_b = Juego.argsBloqueJuego(bloque, args);
+      return `list_push({v:"${v}",a:${arg_b}});\n${v}.push(${args});\n`;
+    } // decir
     return `${a}(${args});\n`;
   };
-  Juego.tiemposBloque[a] = 4;
+  Juego.tiemposBloque[a] = 2;
 }
 
 Juego.argsBloque = function(bloque) {
@@ -72,7 +78,21 @@ Juego.argsBloque = function(bloque) {
     return Mila.generador.valueToCode(bloque, "X", 0);
   }
   return '';
-}
+};
+
+Juego.argsBloqueJuego = function(bloque, defecto) {
+  let arg_b = bloque.getInputTargetBlock('X');
+  if (arg_b === null) {
+    return 'undefined'
+  }
+  if (['listaVacia','lista6Ceros'].includes(arg_b.type)) {
+    return `"${arg_b.type}"`;
+  }
+  if (['album','figuActual','contador','anotador'].includes(arg_b.type)) {
+    return `var_${arg_b.type}`;
+  }
+  return defecto;
+};
 
 // Inicializa todo lo necesario antes de que se termine de cargar la página
 Juego.preCarga = function() {
@@ -109,23 +129,160 @@ Juego.detener = function() {
   }
 };
 
+function crearAlbum(args) {
+  if (Juego.elementos.album.length > 0) {
+    for (let e of Juego.elementos.album) {
+      e.del = true;
+    }
+  } else {
+    Canvas.nuevoObjeto({texto:"Álbum:", x:10, y:120});
+  }
+  Juego.elementos.album = [];
+  if (args === "lista6Ceros") {
+    for (i=0; i<6; i++) {
+      let img = {texto:0, x:10+65*i, y:160, rot:0, scale:2};
+      Juego.elementos.album.push(img);
+      Canvas.nuevoObjeto(img);
+    }
+    for (i=0; i<6; i++) {
+      let index = {texto:i, x:17+65*i, y:180, rot:0, scale:.8};
+      Juego.elementos.album.push(index);
+      Canvas.nuevoObjeto(index);
+    }
+  } else if (typeof args === "number") {
+    let n = {texto:args, x:80, y:120, rot:0};
+    Juego.elementos.album.push(n);
+    Canvas.nuevoObjeto(n);
+  }
+};
+
+function comprarFigu(i) {
+  if (typeof i !== 'number') {
+    i = "?"
+  }
+  if ('figuActual' in Juego.elementos) {
+    Juego.elementos.figuActual.del = true;
+  }
+  Juego.elementos.figuActual = {texto:i, x:120, y:50, scale:2};
+  Canvas.nuevoObjeto(Juego.elementos.figuActual);
+
+};
+
+function asignarContador(args) {
+  if (typeof args !== 'number') {
+    args = "?"
+  }
+  if ('contador' in Juego.elementos) {
+    Juego.elementos.contador.texto = args;
+  } else {
+    let c = {texto:args, x:310, y:45};
+    Juego.elementos.contador = c;
+    Canvas.nuevoObjeto(c);
+    Canvas.nuevoObjeto({texto:"Contador:", x:220, y:45});
+  }
+};
+
+function crearAnotador(args) {
+  if ('anotador' in Juego.elementos) {
+    for (let e of Juego.elementos.anotador) {
+      e.del = true;
+    }
+  } else {
+    Canvas.nuevoObjeto({texto:"Anotador:", x:10, y:260});
+  }
+  Juego.elementos.anotador = [];
+  if (args === "lista6Ceros") {
+    for (i=0; i<6; i++) {
+      anotar("0");
+    }
+  } else if (typeof args === "number") {
+    let n = {texto:args, x:108, y:260, rot:0};
+    Juego.elementos.anotador.push(n);
+    Canvas.nuevoObjeto(n);
+  }
+};
+
+function pegarFigu(pos, i) {
+  if (typeof pos === 'number') {
+    if (pos >= 0 && pos <= 5) {
+      Juego.elementos.album[pos].texto = i;
+    }
+  }
+}
+
+function anotar(args) {
+  if (!('anotador' in Juego.elementos)) {
+    alert("Todavía no creaste el anotador");
+    Mila.detener();
+  } else {
+    let a = {texto:(args === undefined ? '?' : args),
+      x:10+50*(Math.floor(Juego.elementos.anotador.length % 8)),
+      y:285 + 24*(Math.floor(Juego.elementos.anotador.length / 8))
+    };
+    Juego.elementos.anotador.push(a);
+    Canvas.nuevoObjeto(a);
+  }
+};
+
 // Ordena el movimiento de un robot en el mapa
 Juego.mover = function(robot, direccion, args) {
-  let i;
+  let i, v, pos;
   switch (direccion) {
-    case 'crearAlbum':
-      if (Juego.elementos.album.length > 0) {
-        for (let e of Juego.elementos.album) {
-          e.del = true;
-        }
-        Juego.elementos.album = [];
-      } else {
-        Canvas.nuevoObjeto({texto:"Álbum:", x:10, y:120});
+    case 'var_assign':
+      v = args.a.v;
+      args = args.a.a;
+      switch (v) {
+        case 'var_album':
+          crearAlbum(args);
+          break;
+        case 'var_figuActual':
+          comprarFigu(args);
+          break;
+        case 'var_contador':
+          asignarContador(args);
+          break;
+        case 'var_anotador':
+          crearAnotador(args);
+          break;
+        default:
+          //
       }
-      for (i=0; i<6; i++) {
-        let img = {imagen:'vacia', x:10+65*i, y:130, rot:0, scale:.3};
-        Juego.elementos.album.push(img);
-        Canvas.nuevoObjeto(img);
+      break;
+    case 'list_assign':
+      v = args.a.v;
+      pos = args.a.p;
+      args = args.a.a;
+      switch (v) {
+        case 'var_album':
+          if (Juego.elementos.album.length == 0) {
+            alert("Todavía no creaste el álbum");
+            Mila.detener();
+            break;
+          }
+          pegarFigu(pos, args);
+          break;
+        case 'var_anotador':
+          break;
+        default:
+          //
+      }
+      break;
+    case 'list_push':
+      v = args.a.v;
+      args = args.a.a;
+      switch (v) {
+        case 'var_album':
+          if (Juego.elementos.album.length == 0) {
+            alert("Todavía no creaste el álbum");
+            Mila.detener();
+            break;
+          }
+          break;
+        case 'var_anotador':
+          anotar(args);
+          break;
+        default:
+          //
       }
       break;
     case 'album':
@@ -135,82 +292,23 @@ Juego.mover = function(robot, direccion, args) {
         break;
       }
       return undefined;
-    case 'comprarFigu':
-      if ('figuActual' in Juego.elementos) {
-        Juego.elementos.figuActual.del = true;
-      }
-      i = Math.floor(Math.random()*6);
-      Juego.elementos.figuActual = {imagen:`figu${i}`, x:120, y:10, scale:.3, i:i};
-      Canvas.nuevoObjeto(Juego.elementos.figuActual);
-      break;
-    case 'pegarFigu':
+    case 'figuActual':
       if (!('figuActual' in Juego.elementos)) {
-        alert("No tenés ninguna figu para pegar");
+        alert("No tenés ninguna figu");
         Mila.detener();
         break;
       }
-      if (Juego.elementos.album.length == 0) {
-        alert("Todavía no creaste el álbum");
-        Mila.detener();
-        break;
-      }
-      i = Juego.elementos.figuActual.i;
-      Juego.elementos.figuActual.x = 10+65*i;
-      Juego.elementos.figuActual.y = 130;
-      Juego.elementos.album[i].del = true;
-      Juego.elementos.album[i] = Juego.elementos.figuActual;
-      delete Juego.elementos.figuActual;
-      break;
-    case 'crearContador':
-      if ('contador' in Juego.elementos) {
-        Juego.elementos.contador.texto = 0;
-      } else {
-        let c = {texto:0, x:310, y:45};
-        Juego.elementos.contador = c;
-        Canvas.nuevoObjeto(c);
-        Canvas.nuevoObjeto({texto:"Contador:", x:220, y:45});
-      }
-      break;
-    case 'incrementarContador':
-      if (!('contador' in Juego.elementos)) {
-        alert("Todavía no creaste el contador");
-        Mila.detener();
-        break;
-      }
-      Juego.elementos.contador.texto += 1;
-      break;
+      return undefined;
     case 'contador':
       if (!('contador' in Juego.elementos)) {
         alert("Todavía no creaste el contador");
         Mila.detener();
         break;
       }
-      return Juego.elementos.contador.texto;
-      break;
-    case 'crearAnotador':
-      if ('anotador' in Juego.elementos) {
-        for (let e of Juego.elementos.anotador) {
-          e.del = true;
-        }
-      } else {
-        Canvas.nuevoObjeto({imagen:"nota", x:-5, y:255,scale:0.67});
-        Canvas.nuevoObjeto({texto:"Anotador:", x:10, y:260});
-      }
-      Juego.elementos.anotador = [];
+      return undefined;
       break;
     case 'anotar':
-      if (!('anotador' in Juego.elementos)) {
-        alert("Todavía no creaste el anotador");
-        Mila.detener();
-        break;
-      } else {
-        let a = {texto:(args === undefined ? '?' : args),
-          x:10+50*(Math.floor(Juego.elementos.anotador.length % 8)),
-          y:285 + 24*(Math.floor(Juego.elementos.anotador.length / 8))
-        };
-        Juego.elementos.anotador.push(a);
-        Canvas.nuevoObjeto(a);
-      }
+      anotar(args);
       break;
     case 'anotador':
       if (!('anotador' in Juego.elementos)) {
